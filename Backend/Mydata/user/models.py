b8dd5693c1360fa555cf6a32a7d5f29eb5ee9712
ske-kr <54828276+ske-kr@users.noreply.github.com>
2021-04-14 16:43:54 +0900

    user added

--- Backend/Mydata/user/models.py
+++ Backend/Mydata/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    name=models.CharField(null=True, blank=True, max_length=15,default='이름을 설정하지 않았습니다')
+    # already have fields
+    # password, email, first_name, last_name, groups, user_permissions, is_staff, is_active, is_superuser, last_login, date_joined
--- Backend/Mydata/user/views.py
+++ Backend/Mydata/user/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse,HttpResponseNotAllowed
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import auth
+import json
+from .models import User
+from django.contrib.auth import get_user_model, authenticate, login
+from json import JSONDecodeError
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import ensure_csrf_cookie
+import requests
 # Create your views here.
+
+User = get_user_model()
