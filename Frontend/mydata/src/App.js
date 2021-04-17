f418b50f8fcede5ec18630f69b189e88ec81fb50
kim-jos <josephkim828@gmail.com>
2021-04-17 14:31:50 +0900

    프론트 틀
    일단 틀은 짰음

--- Frontend/mydata/src/App.js
+++ Frontend/mydata/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SignIn extends Component {
+  render() {
+    return (
+      <div>
+        <form className='signUp'>
+          <input placeholder='Email' type='email'/>
+          <input placeholder='Password' type='password'/>
+          <button type='submit'>Submit</button>
+        </form>
+        <a href="/SignUp">Sign Up</a>
+      </div>
+    )
+  }
+}
+
+class SignUp extends Component {
+  render() {
+    return (
+      <form className='signUp'>
+        <input placeholder='First Name' type='fName'/>
+        <input placeholder='Last Name' type='LName'/>
+        <input placeholder='Email' type='email'/>
+        <input placeholder='Password' type='password'/>
+        <button type='submit'>Submit</button>
+      </form>
+    )
+  }
+}
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>Profile Picture</div>
+          <a href='/edit'>Edit Profile</a>
+          <a href='#'>Data 불러오기</a>
+          <a href='/'>Logout</a>
+        </div>
+
+        <form className='dataUpload'>
+          <input type="file" id="myFile" name="filename"/>
+          <input type="submit"/>
+        </form>
+        
+      </div>
+    )
+  }
+}
+
+class Edit extends Component {
+  render() {
+    return (
+      <div>
+        <a href='/MainPage'>Main Page</a>
+        <div>
+          <div>Change Name</div>
+          <div>Delete Account</div>
+        </div>
+
+        <form className='dataUpload'>
+          <input type="file" id="myFile" name="filename"/>
+          <input type="submit"/>
+        </form>
+        
+      </div>
+    )
+  }
+}
+
+class Result extends Component {
+  render() {
+    return (
+      <div>
+        <a href='/MainPage'>Main Page</a>
+        <div>
+          Show Data
+        </div>
+
+        <div>
+          Save Data
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Route exact path='/'><SignIn></SignIn></Route>
+        <Route exact path='/Signup'><SignUp></SignUp></Route>
+        <Route exact path='/MainPage'><MainPage></MainPage></Route>
+        <Route exact path='/Edit'><Edit></Edit></Route>
+        <Route exact path='/Result'><Result></Result></Route>
+      </div>
+    )
+  }
 }
 
 export default App;
