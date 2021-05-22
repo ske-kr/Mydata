8932316b2009eff38b33c8403da7700e60784074
kim-jos <josephkim828@gmail.com>
2021-05-22 14:52:55 +0900

    Update App.js
    1. post로 보내는 주소 수정
    2. url 주소 수정

--- Frontend/mydata/src/App.js
+++ Frontend/mydata/src/App.js
@@ -6,7 +6,7 @@ class SignIn extends Component {
   render() {
     return (
       <div>
-        <form action="localhost:8000/user/signin/" method="POST" className='signUp'>
+        <form action="http:://localhost:8000/user/signin/" method="POST" className='signUp'>
           <input placeholder='Email' type='email'/>
           <input placeholder='Password' type='password'/>
           <button type='submit'>Submit</button>
@@ -21,11 +21,11 @@ class SignUp extends Component {
   render() {
     return (
       <div>
-        <form action="localhost:8000/user/signup/" method="POST" className='signUp'>
-          <input placeholder='First Name' type='fName'/>
-          <input placeholder='Last Name' type='LName'/>
-          <input placeholder='Email' type='email'/>
-          <input placeholder='Password' type='password'/>
+        <form action="http:://localhost:8000/user/signup/" method="POST" className='signUp'>
+          <input placeholder='First Name' name='firstname'/>
+          <input placeholder='Last Name' name='lastname'/>
+          <input placeholder='Email' name='username'/>
+          <input placeholder='Password' name='password'/>
           <button type='submit'>Submit</button>
         </form>
         <a href="/">Sign In</a>
@@ -45,7 +45,7 @@ class MainPage extends Component {
           <a href='/'>Logout</a>
         </div>
 
-        <form action="localhost:8000/user/mainpage/" method="POST" className='dataUpload'>
+        <form action="http:://localhost:8000/user/mainpage/" method="POST" className='dataUpload'>
           <input type="file" id="myFile" name="filename"/>
           <input type="submit"/>
         </form>
@@ -60,13 +60,13 @@ class Edit extends Component {
     return (
       <div>
         <a href='/MainPage'>Main Page</a>
-        
+
         <div>
           <div>Change Name</div>
           <div>Delete Account</div>
         </div>
 
-        <form action="localhost:8000/user/dataupload/" method="POST" className='dataUpload'>
+        <form action="http:://localhost:8000/user/dataupload/" method="POST" className='dataUpload'>
           <input type="file" id="myFile" name="filename"/>
           <input type="submit"/>
         </form>
@@ -101,10 +101,10 @@ class App extends Component {
     return (
       <div className='App'>
         <Route exact path='/'><SignIn></SignIn></Route>
-        <Route exact path='/Signup'><SignUp></SignUp></Route>
-        <Route exact path='/MainPage'><MainPage></MainPage></Route>
-        <Route exact path='/Edit'><Edit></Edit></Route>
-        <Route exact path='/Result'><Result></Result></Route>
+        <Route exact path='/signup'><SignUp></SignUp></Route>
+        <Route exact path='/mainpage'><MainPage></MainPage></Route>
+        <Route exact path='/edit'><Edit></Edit></Route>
+        <Route exact path='/result'><Result></Result></Route>
       </div>
     )
   }
