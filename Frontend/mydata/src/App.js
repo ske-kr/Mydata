b5c3351db7ea1015bb808190feff68a022e8c72c
ske-kr <54828276+ske-kr@users.noreply.github.com>
2021-05-22 15:58:53 +0900

    Update App.js

--- Frontend/mydata/src/App.js
+++ Frontend/mydata/src/App.js
@@ -6,11 +6,7 @@ class SignIn extends Component {
   render() {
     return (
       <div>
-<<<<<<< Updated upstream
         <form action="http:://localhost:8000/user/signin/" method="POST" className='signUp'>
-=======
-        <form action="localhost:8000/user/signup/" method="POST" className='signUp'>
->>>>>>> Stashed changes
           <input placeholder='Email' type='email'/>
           <input placeholder='Password' type='password'/>
           <button type='submit'>Submit</button>
@@ -25,11 +21,7 @@ class SignUp extends Component {
   render() {
     return (
       <div>
-<<<<<<< Updated upstream
-        <form action="http:://localhost:8000/user/signup/" method="POST" className='signUp'>
-=======
         <form action="http://localhost:8000/user/signup/" method="POST" className='signUp'>
->>>>>>> Stashed changes
           <input placeholder='First Name' name='firstname'/>
           <input placeholder='Last Name' name='lastname'/>
           <input placeholder='Email' name='username'/>
